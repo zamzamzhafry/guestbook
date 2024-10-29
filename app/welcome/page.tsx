a19ff0702c373f58a7c3e2748c26b9da9c313fb1
zamzamzhafry <47729327+zamzamzhafry@users.noreply.github.com>
2024-10-29 22:11:21 +0700

    revisi suspense

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,21 +1,34 @@
 // app/welcome/page.tsx
 'use client';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+// import { Button } from 'ui';
 
-export default function WelcomePage() {
+const WelcomeContent = () => {
   const searchParams = useSearchParams();
-  const value = searchParams.get("value") || "No value provided";
-
+  const value = searchParams.get("value") || "Tidak ada nama";
   const genderParam = searchParams.get("gender");
-   const genderValue = genderParam === "female" ? "nyonya" : genderParam === "male" ? "tuan" : "No gender provided";
+  const genderValue =
+    genderParam === "female" ? "nyonya" : genderParam === "male" ? "tuan" : "Tidak ada Gender";
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-8">
-      <button className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4' onClick={() => window.location.href = "/"}> Kembali ke Menu</button>
-      <h1 className="text-2xl font-bold mb-4 text-white">Selamat Datang</h1>
+      <h1 className="text-2xl font-bold mb-4 text-white">Welcome Page</h1>
       <p className="text-lg text-white">
         {genderValue} : <span className="font-mono">{value}</span>
       </p>
     </div>
+  );
+};
+
+export default function WelcomePage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className='flex justify-center'>
+      <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => window.history.back()}>Back Home</button>
+
+      </div>
+      <WelcomeContent />
+    </Suspense>
   );
 }
