d5b70be3f15ee5dbc769c84919666c93d67b003a
zamzamzhafry <47729327+zamzamzhafry@users.noreply.github.com>
2024-10-29 22:27:43 +0700

    remove link

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 // import Image from "next/image";
 import QRCode from "react-qr-code";
-import Link from "next/link";
+// import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
