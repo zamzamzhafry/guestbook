6ed261bd443b2b4fa8613ba2dbe560085cea7312
zamzamzhafry <47729327+zamzamzhafry@users.noreply.github.com>
2024-11-01 16:53:13 +0700

    shadcn fix.

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,123 @@
 'use client'
 // import Image from "next/image";
-import QRCode from "react-qr-code";
-// import Link from "next/link";
-import { useState } from "react";
+import QRCode from 'react-qr-code'
+import Link from 'next/link'
+import { useState } from 'react'
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState("");
-  const [inputGenderValue, setInputGenderValue] = useState("");
+    const [inputValue, setInputValue] = useState('')
+    // const [inputGenderValue, setInputGenderValue] = useState('')
 
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:3000"; // Fallback for local development
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://localhost:3000' // Fallback for local development
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setInputValue(value);
-  };
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value
+        setInputValue(value)
+    }
 
-  const handleChangeGender = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value || "male";
-    setInputGenderValue(value);
-  };
+    const [inputGenderValue, setInputGenderValue] = useState('male')
 
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="grid grid-rows-[1fr_1fr]">
-        <h3 className="text-center sm:text-left text-foreground">Ketik teks untuk generate QR</h3>
-        <div className="pb-4">
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleChange}
-          placeholder="Type something here"
-          className="text-foreground"
-        />
+    const handleChangeGender = (value: string) => {
+        setInputGenderValue(value)
+    }
+
+    return (
+        <div className="flex justify-center justify-items-center h-screen">
+            <main className="flex flex-col items-center justify-center">
+                <Card className="w-[350px]">
+                    <CardHeader>
+                        <CardTitle>QR Generator</CardTitle>
+                        <CardDescription>
+                            Masukkan Nama dan Gender
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div className="grid w-full items-center gap-4">
+                                <div className="flex flex-col space-y-1.5">
+                                    <Label htmlFor="name">Name</Label>
+                                    <Input
+                                        id="name"
+                                        placeholder="Nama Hadirin"
+                                        value={inputValue}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="flex flex-col space-y-1.5">
+                                    <Label htmlFor="framework">
+                                        Pilih Gender
+                                    </Label>
+                                    <Select
+                                        value={inputGenderValue || 'male'}
+                                        onValueChange={handleChangeGender}
+                                        // onChange={handleChangeGender}
+                                    >
+                                        <SelectTrigger id="framework">
+                                            <SelectValue placeholder="Pilih Gender" />
+                                        </SelectTrigger>
+                                        <SelectContent position="popper">
+                                            <SelectItem value="male">
+                                                Bapak/Sdr.
+                                            </SelectItem>
+                                            <SelectItem value="female">
+                                                Ibu/Sdri.
+                                            </SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                                <div className="pb-6 pl-4 justify-center">
+                                    <Link
+                                        href={
+                                            inputValue
+                                                ? `${baseUrl}/welcome?value=${encodeURIComponent(
+                                                      inputValue
+                                                  )}&gender=${encodeURIComponent(
+                                                      inputGenderValue
+                                                  )}`
+                                                : ''
+                                        }
+                                    >
+                                        <QRCode
+                                            value={
+                                                inputValue
+                                                    ? `${baseUrl}/welcome?value=${encodeURIComponent(
+                                                          inputValue
+                                                      )}&gender=${encodeURIComponent(
+                                                          inputGenderValue
+                                                      )}`
+                                                    : ''
+                                            }
+                                        />
+                                    </Link>
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="flex justify-between">
+                        <Button variant="outline">Cancel</Button>
+                        <Button>Deploy</Button>
+                    </CardFooter>
+                </Card>
+            </main>
         </div>
-        <select name="" id="" value={inputGenderValue} onChange={handleChangeGender} className="text-foreground">
-          <option value="male" >Bapak/Sdr</option>
-          <option value="female">Ibu/Nona</option>
-        </select>
-      </main>
-      
- <QRCode
-          value={inputValue ? `${baseUrl}/welcome?value=${encodeURIComponent(inputValue)}&gender=${encodeURIComponent(inputGenderValue)}` : ""}
-        />
-
-    </div>
-  );
+    )
 }
--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,34 +1,42 @@
 // app/welcome/page.tsx
-'use client';
-import { useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
-// import { Button } from 'ui';
+'use client'
+import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
+import { Button } from '@/components/ui/button'
 
 const WelcomeContent = () => {
-  const searchParams = useSearchParams();
-  const value = searchParams.get("value") || "Tidak ada nama";
-  const genderParam = searchParams.get("gender");
-  const genderValue =
-    genderParam === "female" ? "nyonya" : genderParam === "male" ? "tuan" : "Tidak ada Gender";
+    const searchParams = useSearchParams()
+    const value = searchParams.get('value') || 'Tidak ada nama'
+    const genderParam = searchParams.get('gender')
+    const genderValue =
+        genderParam === 'female'
+            ? 'nyonya'
+            : genderParam === 'male'
+            ? 'tuan'
+            : 'Tidak ada Gender'
 
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-8">
-      <h1 className="text-2xl font-bold mb-4 text-white">Welcome Page</h1>
-      <p className="text-lg text-white">
-        {genderValue} : <span className="font-mono">{value}</span>
-      </p>
-    </div>
-  );
-};
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center p-8">
+            <h1 className="text-2xl font-bold mb-4 text-white">Welcome Page</h1>
+            <p className="text-lg text-white">
+                {genderValue} : <span className="font-mono">{value}</span>
+            </p>
+        </div>
+    )
+}
 
 export default function WelcomePage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className='flex justify-center'>
-      <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => window.history.back()}>Back Home</button>
-
-      </div>
-      <WelcomeContent />
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="flex justify-center">
+                <Button
+                    className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => window.history.back()}
+                >
+                    Back Home
+                </Button>
+            </div>
+            <WelcomeContent />
+        </Suspense>
+    )
 }
